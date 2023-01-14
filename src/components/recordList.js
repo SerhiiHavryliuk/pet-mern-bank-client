a6b91a3022b2f8c58386c80d1cf44a9c30a1af43
SerhiiHavryliuk <havryliuk.serhii@gmail.com>
2023-01-14 15:53:44 +0200

    added Production mode - url server

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -25,7 +25,10 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://localhost:5000/record/`);
+      // Development mode
+      // const response = await fetch(`http://localhost:5000/record/`);
+      // Production mode
+      const response = await fetch(`https://bank-server-lbi6.onrender.com/`);
 
       if (!response.ok) {
         const message = `An error occured: ${response.statusText}`;
